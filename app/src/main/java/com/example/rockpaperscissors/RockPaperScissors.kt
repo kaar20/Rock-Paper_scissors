0171782dc2310f99a83cb0f7094297de7953134b
kaar20 <codewithkar@gmail.com>
2024-11-17 15:12:47 +0300

    updated

--- app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -1,54 +1,79 @@
 package com.example.rockpaperscissors
 
 fun main(){
-    var computerChoice =""
-    var playerChoice = ""
-    println("Rock, Paper, Scissors? Enter your choice")
-val playerChoiceByNumber = readln().toInt()
-    playerChoice = when(playerChoiceByNumber){
-        1 -> {
-            "Rock"
-        }
-        2 -> {
-            "Paper"
-        }
-        3 -> {
-            "Scissors"
-        }
-        else -> {
-            "Naga-la Tag waxaa Wadid"
-        }
+
+ /*   var again = readln().toBoolean()
+    while (playerChoiceByNumber in 1..3)
+    {
+        when(again)
+        {
+            true-> playerChoiceByNumber = readln().toInt()
+
+        else ->
+            again =false
 
     }
-    val randomNumber = (1..3).random()
+        */
+
+//    }
+
+    var again = true
+    while (again){
+        var computerChoice =""
+        var playerChoice = ""
+        println("Rock, Paper, Scissors? Enter your choice")
+        var playerChoiceByNumber = readln().toInt()
+
+        playerChoice = when(playerChoiceByNumber){
+            1 -> {
+                "Rock"
+            }
+            2 -> {
+                "Paper"
+            }
+            3 -> {
+                "Scissors"
+            }
+            else -> {
+                "Naga-la Tag waxaa Wadid"
+            }
 
 
-    computerChoice = when (randomNumber) {
-        1 -> {
-            "Rock"
         }
-        2 -> {
-            "Paper"
-        }
-        3 -> {
-            "Scissors"
-        }
-        else -> {
-            "Naga-la Tag waxaa Wadid"
-        }
-    }
-    println(computerChoice)
-    println(playerChoice)
 
-    val winner = when {
-        computerChoice == playerChoice ->"Tie"
-        playerChoice =="Rock" && computerChoice =="Scissors" ->"Player"
-        playerChoice =="Paper"&& computerChoice=="Rock" ->"Player"
-        playerChoice == "Scissors"&& computerChoice =="Paper" ->"Player"
-        else->"Computer"
-    }
-    when (winner){
-        "Tie"-> println("its Draw")
-        else-> println("$winner Won!")
+        val randomNumber = (1..3).random()
+
+
+        computerChoice = when (randomNumber) {
+            1 -> {
+                "Rock"
+            }
+            2 -> {
+                "Paper"
+            }
+            3 -> {
+                "Scissors"
+            }
+            else -> {
+                "Naga-la Tag waxaa Wadid"
+            }
+        }
+        println(computerChoice)
+        println(playerChoice)
+
+        val winner = when {
+            computerChoice == playerChoice ->"Tie"
+            playerChoice =="Rock" && computerChoice =="Scissors" ->"Player"
+            playerChoice =="Paper"&& computerChoice=="Rock" ->"Player"
+            playerChoice == "Scissors"&& computerChoice =="Paper" ->"Player"
+            else->"Computer"
+        }
+        when (winner){
+            "Tie"-> println("its Draw")
+            else-> println("$winner Won!")
+        }
+        println("Try again :true or false")
+        again= readln().toBoolean()
+
     }
 }
